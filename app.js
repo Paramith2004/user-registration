2ce0e01e603b7506f44db0a091650750653072ae
Paramith Kavisha <paramithkavisha@gmail.com>
2025-10-10 21:54:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,18 +5,14 @@ const userRoutes = require('./routes/userRoutes'); // Make sure this path is cor
 
 const app = express();
 
-// Middleware
 app.use(bodyParser.json());
 
-// Use the routes from userRoutes.js
 app.use('/', userRoutes);
 
-// Connect to MongoDB
 mongoose.connect('mongodb://127.0.0.1:27017/userDB')
     .then(() => console.log('✅ MongoDB Connected'))
     .catch(err => console.log(err));
 
-// Start the server
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
