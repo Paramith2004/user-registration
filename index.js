91e52ed24a348c249f9b7dd4c2ca48c9a0dfce74
Paramith Kavisha <paramithkavisha@gmail.com>
2025-10-10 21:59:43 +0530

    Add main server entry point (index.js) with MongoDB connection setup

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
-const session = require('express-session');
+const session = require('express-session'); 
 const User = require('./models/user');
 const userRoutes = require('./routes/userRoutes');
 
